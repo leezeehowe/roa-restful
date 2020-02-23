03c1b18a656f40ce0ce3c66283c63b586ac91f80
leezeehowe <leezeehowe@gmail.com>
2020-02-23 10:27:47 +0800

    add .npmignore

--- index.js
+++
@@ -1,7 +0,0 @@
-const { Get, Put, Post, Delete, Controller } = require("./src/roa-resuful-decorator");
-const { Roa } = require("./src/roa-store");
-
-module.exports =
-{
-    Get, Put, Post, Delete, Controller, Roa
-}
